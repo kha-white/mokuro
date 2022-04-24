e9ea206995af2f1cb2ae68740df9a235a73a3c7d
Maciej Budyś <kha-white@mail.com>
2022-04-24 20:44:10 +0200

    0.1.1 release

--- mokuro/__init__.py
+++ mokuro/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from mokuro.manga_page_ocr import MangaPageOcr
 from mokuro.overlay_generator import OverlayGenerator
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (Path(__file__).parent / "README.md").read_text('utf-8').spli
 
 setup(
     name="mokuro",
-    version='0.1.0',
+    version='0.1.1',
     description="Browser reader for manga with selectable text",
     long_description=long_description,
     long_description_content_type="text/markdown",
