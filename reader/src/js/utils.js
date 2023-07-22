d71d0dde70200d7b69d573ac2a647d102cad4d39
Maciej Budyś <kha-white@mail.com>
2023-07-22 14:52:22 +0200

    remove unused code

--- reader/src/js/utils.js
+++ reader/src/js/utils.js
@@ -68,23 +68,3 @@ export async function readJSON(file) {
     let text = await file.text();
     return JSON.parse(text);
 }
-
-export async function verifyPermission(fileHandle, withWrite) {
-    const opts = {};
-    if (withWrite) {
-        opts.mode = 'readwrite';
-    }
-
-    // Check if we already have permission, if so, return true.
-    if (await fileHandle.queryPermission(opts) === 'granted') {
-        return true;
-    }
-
-    // Request permission to the file, if the user grants permission, return true.
-    if (await fileHandle.requestPermission(opts) === 'granted') {
-        return true;
-    }
-
-    // The user did not grant permission, return false.
-    return false;
-}
