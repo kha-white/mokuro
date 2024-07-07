0283cd825c2a2b1ced3b8c3bf184027b5e7312f6
Maciej Budyś <kha-white@mail.com>
2024-07-07 11:36:54 +0200

    replace setup.py with pyproject.toml

--- mokuro/__init__.py
+++ mokuro/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0-beta.7'
+from ._version import __version__ as __version__
 
-from mokuro.manga_page_ocr import MangaPageOcr
-from mokuro.mokuro_generator import MokuroGenerator
+from mokuro.manga_page_ocr import MangaPageOcr as MangaPageOcr
+from mokuro.mokuro_generator import MokuroGenerator as MokuroGenerator
