71cb25dc65750c545f9e4e52f29bfb2cd9f84631
Maciej Budyś <kha-white@mail.com>
2024-07-09 16:24:21 +0200

    add formatting error

--- mokuro/__init__.py
+++ mokuro/__init__.py
@@ -1,4 +1,4 @@
-from ._version import __version__ as __version__
+from ._version import __version__ as __version__ 
 
 from mokuro.manga_page_ocr import MangaPageOcr as MangaPageOcr
 from mokuro.mokuro_generator import MokuroGenerator as MokuroGenerator
